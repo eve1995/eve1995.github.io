0a82201e5163414e5fbf96c1934b0ff7ec4016ad
eve1995 <lam13023@byui.edu>
2020-01-25 17:52:19 -0700

    lesson 3

---
+++ js/main.js
@@ -0,0 +1,4 @@
+onload = function(){
+    document.getElementById("lastModified").innerHTML = document.lastModified;
+    document.getElementById("copyright-year").innerHTML = new Date().getFullYear();            
+}
