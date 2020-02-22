cc9a0387c79568d944f9c60ad31213df16b98978
eve1995 <lam13023@byui.edu>
2020-02-21 19:43:09 -0700

    lesson 7 gallery page

---
+++ lesson7/js/main.js
@@ -0,0 +1,25 @@
+onload = function(){
+    document.getElementById("lastModified").innerHTML = formatDate(new Date());
+    document.getElementById("copyright-year").innerHTML = new Date().getFullYear();       
+}
+
+function formatDate(date) {
+    const monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+
+    const dayNames = [
+        "Sunday", "Monday", "Tuesday", 
+        "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+  
+    const dayOfWeek = date.getDay();
+    const day = date.getDate();
+    const monthIndex = date.getMonth();
+    const year = date.getFullYear();
+  
+    return dayNames[dayOfWeek] + ", " + day + " " + monthNames[monthIndex] + " " + year;
+  }
