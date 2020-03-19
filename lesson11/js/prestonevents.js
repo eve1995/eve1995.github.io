9c19fe3b2786116650e6daa0d31f2b6b39fbf2b9
eve1995 <lam13023@byui.edu>
2020-03-18 21:46:57 -0600

    fixed images

--- lesson11/js/prestonevents.js
+++ lesson11/js/prestonevents.js
@@ -5,8 +5,23 @@ fetch(prestonEventsURL)
   .then((jsObject) => {
     console.log(jsObject);
     for (let i = 0; i < towns.length; i++ ) {
-      if (pagetowns.includes(towns[i].name))
-      {
-      }
+        if (pagetowns.includes(towns[i].name))
+        {
+            // Get the variable from the JSON file (array)
+            const eventArray = towns[i].events;
+            
+            // loop over all elements
+            eventArray.forEach(element => 
+            {
+                // 1. Create a variable: 
+                let divEvent = document.createElement('div');
+
+                // 2. Set the content of the div:  
+                divEvent.TextContent = element;
+
+                // 3. Append div to event-list: 
+                document.getElementById('event-list').appendChild(divEvent);
+            }); 
+        }
     }    
   });
